2c5ba12767641dbd6b13387a65f5bfcfdaffd045
DARKWOLF <jabulanichizimba1@gmail.com>
2024-07-24 11:13:59 +0200

    deleted a table from the database

--- resources/views/manager/mhome.blade.php
+++ resources/views/manager/mhome.blade.php
@@ -6,6 +6,15 @@
 
     <a href="{{route('create_page')}}"><button>creation page</button></a>
     
+    @foreach ($table_data as $data)
+      <table>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Description</th>
+        <th>price</th>
+        <th>Image</th>
+      </table>
+    @endforeach
 
   </div>
 
