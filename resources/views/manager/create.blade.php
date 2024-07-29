aa05d54f93d21056b8673b6e9ba251add7a1a7e7
DARKWOLF <jabulanichizimba1@gmail.com>
2024-07-29 10:29:30 +0200

    made the display page for the user, added borders to manager page and changed input tag to textarea tag in the create page and edit paga

--- resources/views/manager/create.blade.php
+++ resources/views/manager/create.blade.php
@@ -1,17 +1,87 @@
 <title>Item Creation</title>
 
+<style>
+  .Cwrapper-main
+  {
+    margin: auto;
+    width: 500px;
+  }
+
+  body
+  {
+
+    font-family: 'Franklin Gothic Medium';
+    font-size: 18;
+    text-align: center;
+    margin: 10px;
+    background-color: blueviolet;
+
+  }
+
+  button
+  {
+    background-color: #ccc;
+  }
+
+  button:hover
+  {
+    background-color: hotpink;
+    cursor: pointer;
+  }
+
+  textarea, input
+  {
+    border: 3px solid #ccc;
+  }
+
+  textarea:focus, input[type="text"]:focus
+  {
+    outline: 3px solid purple;
+  }
+
+  textarea
+  {
+    width: 25%;
+    height: 25%;
+    resize: both;
+  }
+
+  input[type="text"],textarea,input[type="number"]
+  {
+    background-color: rgb(153, 235, 135);
+  }
+
+  input[type="text"],textarea,input[type="number"],button
+  {
+    padding: 10px;
+    border-radius: 10px;
+  }
+</style>
+
 <body>
 
-  <div class="Cwapper">
+  <section class="Cwrapper">
     <h2> Add product</h2>
     <form action="{{route('data_storage')}}" method="POST">
       @csrf
-      <input type="text" name="Name" placeholder="Name"> 
-      <input type="text" name="description" placeholder="description">
-      <input type="text" name="price" placeholder="price">
+      <label for="Name"> Name </label>
+      <br>
+      <input type="text" id="Name" name="Name" placeholder="Name" required>
+      <br><br>
+
+      <label for="Description"> Description </label> 
+      <br>
+      <textarea id="Description" name="description" placeholder="description" required></textarea>
+      <br><br>
+
+      <label for="Price"> Price </label>
+      <br>
+      <input type="text" id="price" name="price" placeholder="0.00" required>
+      <br><br>
+
       <button type="submit">Add</button>
  
 
-  </div>
+    </section>
   
 </body>
